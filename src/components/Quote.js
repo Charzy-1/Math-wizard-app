bab9a9a99b684811375da4c6c44632867a2d4129
Charles <chazylites@gmail.com>
2024-08-21 14:49:01 +0100

    Changed the color of navbar brand

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const Quote = () => (
-  <div className='Quote-container'>
-    <p>Mathematics is not all about numbers, equations, computations, or algorithms: 
-        It is about understanding - Williams Paul Thurston
+  <div className="Quote-container">
+    <p>
+      Mathematics is not all about numbers, equations, computations, or algorithms:
+      It is about understanding -
+      <strong>Williams Paul Thurston</strong>
     </p>
   </div>
 );
